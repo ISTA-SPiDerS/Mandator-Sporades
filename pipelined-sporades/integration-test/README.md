7828dfd4a675e360240dc206f53d9e2c1e7c408b
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-06-16 18:51:25 +0200

    checked integration-test/ files; all files checked; local coding finished

--- pipelined-sporades/integration-test/README.md
+++ pipelined-sporades/integration-test/README.md
@@ -2,6 +2,4 @@ This directory contains
 
 (1) bash scripts to run local tests
 
-(2) python scripts to check for correctness and simulate attacks
-
-To run the complete set of integration-tests in a single machine ```python3 integration-test/python/test-automation.py```
+(2) python scripts to check for correctness
--- pipelined-sporades/integration-test/safety_test.sh
+++ pipelined-sporades/integration-test/safety_test.sh
@@ -1,18 +1,13 @@
 arrivalRate=$1
-algo=$2
-viewTimeoutTime=$3
-batchTime=$4
-batchSize=$5
-pipelineLength=$6
-window=$7
+viewTimeoutTime=$2
+batchTime=$3
+batchSize=$4
+pipelineLength=$5
+window=$6
 
 replica_path="replica/bin/replica"
 ctl_path="client/bin/client"
-output_path="logs/${arrivalRate}/${algo}/${viewTimeoutTime}/${batchTime}/${batchSize}/${pipelineLength}/${window}/"
-
-rm -r configuration/local
-mkdir -p configuration/local
-python3 configuration/config-generate.py 5 5 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 > configuration/local/configuration.yml
+output_path="logs/${arrivalRate}/${viewTimeoutTime}/${batchTime}/${batchSize}/${pipelineLength}/${window}/"
 
 rm -r ${output_path}
 mkdir -p ${output_path}
@@ -22,11 +17,11 @@ pkill client; pkill client; pkill client; pkill client; pkill client
 
 echo "Killed previously running instances"
 
-nohup ./${replica_path} --name 1 --consAlgo "${algo}" --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 100 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}1.log &
-nohup ./${replica_path} --name 2 --consAlgo "${algo}" --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 100 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}2.log &
-nohup ./${replica_path} --name 3 --consAlgo "${algo}" --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 100 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}3.log &
-nohup ./${replica_path} --name 4 --consAlgo "${algo}" --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 100 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}4.log &
-nohup ./${replica_path} --name 5 --consAlgo "${algo}" --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 100 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}5.log &
+nohup ./${replica_path} --name 1 --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 0 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}1.log &
+nohup ./${replica_path} --name 2 --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 0 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}2.log &
+nohup ./${replica_path} --name 3 --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 0 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}3.log &
+nohup ./${replica_path} --name 4 --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 0 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}4.log &
+nohup ./${replica_path} --name 5 --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 0 --viewTimeout "${viewTimeoutTime}" --pipelineLength "${pipelineLength}" --logFilePath ${output_path} >${output_path}5.log &
 
 echo "Started 5 replicas"
 
@@ -46,18 +41,18 @@ echo "sent consensus start up status"
 
 echo "starting clients"
 
-nohup ./${ctl_path} --name 21 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 100 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}21.log &
-nohup ./${ctl_path} --name 22 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 100 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}22.log &
-nohup ./${ctl_path} --name 23 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 100 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}23.log &
-nohup ./${ctl_path} --name 24 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 100 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}24.log &
-nohup ./${ctl_path} --name 25 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 100 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}25.log &
+nohup ./${ctl_path} --name 21 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 0 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}21.log &
+nohup ./${ctl_path} --name 22 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 0 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}22.log &
+nohup ./${ctl_path} --name 23 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 0 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}23.log &
+nohup ./${ctl_path} --name 24 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 0 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}24.log &
+nohup ./${ctl_path} --name 25 --logFilePath ${output_path} --requestType request --debugOn --debugLevel 0 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --window "${window}" >${output_path}25.log &
 
-sleep 120
+sleep 80
 
 echo "finished running clients"
 
 
-nohup ./${ctl_path} --name 21 --logFilePath ${output_path} --requestType status --operationType 2  --debugOn --debugLevel 10 >${output_path}status2.log &
+nohup ./${ctl_path} --name 21 --logFilePath ${output_path} --requestType status --operationType 2  --debugOn --debugLevel 0 >${output_path}status2.log &
 
 
 echo "sent status to print logs"
@@ -66,7 +61,6 @@ sleep 30
 
 pkill replica; pkill replica; pkill replica; pkill replica; pkill replica
 pkill client; pkill client; pkill client; pkill client; pkill client
-rm -r configuration/local
 
 python3 integration-test/python/overlay-test.py ${output_path}/1-consensus.txt ${output_path}/2-consensus.txt ${output_path}/3-consensus.txt ${output_path}/4-consensus.txt ${output_path}/5-consensus.txt
 
