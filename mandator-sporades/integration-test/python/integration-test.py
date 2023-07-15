b9e4277807052ea1e18a0d29b987439ee516fe39
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-07-15 20:34:53 +0200

    all files checked; running automated local tests

--- mandator-sporades/integration-test/python/integration-test.py
+++ mandator-sporades/integration-test/python/integration-test.py
@@ -1,22 +1,64 @@
 import os
 
-arrivalRate = [10, 1000, 10000]
-viewTimeoutTime = [1000, 10000, 15000, 300000000]
-batchTime = [1000, 5000]
-batchSize = [1, 50]
-networkBatchTime = [3, 5, 10]
-asyncSimTime = [1, 5, 10]
-consAlgo = ["paxos", "async"]
-mode = [1, 2]
+# no batching sync
+arr = 30
+viewTimeout = 30000000
+batchTi = 5000
+batchSi = 1
+networkBatchTim = 3
+asyncSimTim = 0
 
-for arr in arrivalRate:
-    for viewTimeout in viewTimeoutTime:
-        for batchTi in batchTime:
-            for batchSi in batchSize:
-                for networkBatchTim in networkBatchTime:
-                    for asyncSimTim in asyncSimTime:
-                        for cons in consAlgo:
-                            for m in mode:
-                                os.system("/bin/bash integration-test/safety_test.sh " + str(arr) + " " + str(
-                                    viewTimeout) + " " + str(batchTi) + " " + str(batchSi) + " " + str(
-                                    networkBatchTim) + " " + str(asyncSimTim) + " " + str(cons) + " " + str(m))
+for cons in ["paxos", "async"]:
+    for m in [1, 2]:
+        os.system("/bin/bash integration-test/safety_test.sh " + str(arr) + " " + str(
+            viewTimeout) + " " + str(batchTi) + " " + str(batchSi) + " " + str(
+            networkBatchTim) + " " + str(asyncSimTim) + " " + str(cons) + " " + str(m))
+
+# batching sync
+arr = 10000
+viewTimeout = 30000000
+batchTi = 5000
+batchSi = 50
+networkBatchTim = 3
+asyncSimTim = 0
+
+for cons in ["paxos", "async"]:
+    for m in [1, 2]:
+        os.system("/bin/bash integration-test/safety_test.sh " + str(arr) + " " + str(
+            viewTimeout) + " " + str(batchTi) + " " + str(batchSi) + " " + str(
+            networkBatchTim) + " " + str(asyncSimTim) + " " + str(cons) + " " + str(m))
+
+# low async with batching
+
+arr = 10000
+viewTimeout = 12000
+batchTi = 5000
+batchSi = 50
+networkBatchTim = 3
+asyncSimTim = 7
+
+for cons in ["paxos", "async"]:
+    for m in [1, 2]:
+        os.system("/bin/bash integration-test/safety_test.sh " + str(arr) + " " + str(
+            viewTimeout) + " " + str(batchTi) + " " + str(batchSi) + " " + str(
+            networkBatchTim) + " " + str(asyncSimTim) + " " + str(cons) + " " + str(m))
+
+
+# high async with batching
+arr = 10000
+viewTimeout = 12000
+batchTi = 5000
+batchSi = 50
+networkBatchTim = 3
+asyncSimTim = 15
+
+for cons in ["paxos", "async"]:
+    for m in [1, 2]:
+        os.system("/bin/bash integration-test/safety_test.sh " + str(arr) + " " + str(
+            viewTimeout) + " " + str(batchTi) + " " + str(batchSi) + " " + str(
+            networkBatchTim) + " " + str(asyncSimTim) + " " + str(cons) + " " + str(m))
+
+
+
+
+
