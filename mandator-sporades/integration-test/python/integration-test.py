9b9b4674c4757e98f713ddb098d94e4b761f5a04
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-07-15 20:21:41 +0200

    all files checked

--- mandator-sporades/integration-test/python/integration-test.py
+++ mandator-sporades/integration-test/python/integration-test.py
@@ -1,13 +1,13 @@
 import os
 
-arrivalRate=[10, 1000, 10000]
-viewTimeoutTime=[1000, 10000, 15000, 300000000]
-batchTime=[1000, 5000]
-batchSize=[1, 50]
-networkBatchTime=[3, 5, 10]
-asyncSimTime=[1, 5, 10]
-consAlgo=["paxos", "async"]
-mode=[1, 2]
+arrivalRate = [10, 1000, 10000]
+viewTimeoutTime = [1000, 10000, 15000, 300000000]
+batchTime = [1000, 5000]
+batchSize = [1, 50]
+networkBatchTime = [3, 5, 10]
+asyncSimTime = [1, 5, 10]
+consAlgo = ["paxos", "async"]
+mode = [1, 2]
 
 for arr in arrivalRate:
     for viewTimeout in viewTimeoutTime:
@@ -17,4 +17,6 @@ for arr in arrivalRate:
                     for asyncSimTim in asyncSimTime:
                         for cons in consAlgo:
                             for m in mode:
-                                os.system("/bin/bash integration-test/safety_test.sh "+str(arr)+" "+str(viewTimeout)+" "+str(batchTi)+" "+str(batchSi)+" "+str(networkBatchTim)+" "+str(asyncSimTim)+" "+str(cons)+" "+str(m))
+                                os.system("/bin/bash integration-test/safety_test.sh " + str(arr) + " " + str(
+                                    viewTimeout) + " " + str(batchTi) + " " + str(batchSi) + " " + str(
+                                    networkBatchTim) + " " + str(asyncSimTim) + " " + str(cons) + " " + str(m))
