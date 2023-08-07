b8de76019c3d0c0aad588533d46688eeb93c486b
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-08-07 15:46:01 +0200

    checked experiments/best-case/paxos_raft.sh

--- experiments/best-case/paxos_raft.sh
+++ experiments/best-case/paxos_raft.sh
@@ -3,31 +3,24 @@ replicaBatchSize=$2
 replicaBatchTime=$3
 setting=$4 # LAN or WAN
 iteration=$5
-
-pipelineLength=1
-if [[ "${setting}" == "WAN" ]]
-then
-  pipelineLength=10
-fi
+pipelineLength=$6
+algo=$7
 
 pwd=$(pwd)
 . "${pwd}"/experiments/setup-5/ip.sh
 
-remote_algo_path="/raxos/binary/paxos_raft_repl"
-remote_ctl_path="/raxos/binary/paxos_raft_clie"
-remote_log_path="/home/${user_name}/raxos/logs/"
+remote_algo_path="/mandator/binary/pa_ra_replica"
+remote_ctl_path="/mandator/binary/pa_ra_client"
 
-remote_config_path="/home/${user_name}/raxos/binary/paxos.yml"
+
+remote_config_path="/home/${user_name}/mandator/binary/paxos.yml"
 
 echo "Starting test"
 
-output_path="${pwd}/experiments/best-case/logs/paxos-v2/${arrival}/${replicaBatchSize}/${replicaBatchTime}/${setting}/${iteration}/execution/"
+output_path="${pwd}/experiments/best-case/logs/${algo}/${arrival}/${replicaBatchSize}/${replicaBatchTime}/${setting}/${iteration}/${pipelineLength}/execution/"
 rm -r "${output_path}" ; mkdir -p "${output_path}"
 
 echo "Removed old local log files"
-
-reset_logs="rm -r ${remote_log_path} ; mkdir -p ${remote_log_path}"
-kill_command="pkill epaxos_master ; pkill epaxos_server; pkill epaxos_client; pkill paxos_raft_repl ; pkill paxos_raft_clie; pkill quepaxa_replica ; pkill quepaxa_client; pkill rabia"
 
 for i in "${machines[@]}"
 do
@@ -40,11 +33,11 @@ echo "Removed all files in remote servers"
 
 sleep 2
 
-nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica1} ".${remote_algo_path} --name 1 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo paxos --pipelineLength ${pipelineLength}  " >${output_path}1.log &
-nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica2} ".${remote_algo_path} --name 2 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo paxos --pipelineLength ${pipelineLength}  " >${output_path}2.log &
-nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica3} ".${remote_algo_path} --name 3 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo paxos --pipelineLength ${pipelineLength}  " >${output_path}3.log &
-nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica4} ".${remote_algo_path} --name 4 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo paxos --pipelineLength ${pipelineLength}  " >${output_path}4.log &
-nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica5} ".${remote_algo_path} --name 5 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo paxos --pipelineLength ${pipelineLength}  " >${output_path}5.log &
+nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica1} ".${remote_algo_path} --name 1 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo ${algo} --pipelineLength ${pipelineLength}  " >${output_path}1.log &
+nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica2} ".${remote_algo_path} --name 2 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo ${algo} --pipelineLength ${pipelineLength}  " >${output_path}2.log &
+nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica3} ".${remote_algo_path} --name 3 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo ${algo} --pipelineLength ${pipelineLength}  " >${output_path}3.log &
+nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica4} ".${remote_algo_path} --name 4 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo ${algo} --pipelineLength ${pipelineLength}  " >${output_path}4.log &
+nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} -n -f ${replica5} ".${remote_algo_path} --name 5 --config ${remote_config_path}  --logFilePath ${remote_log_path} --batchSize ${replicaBatchSize} --batchTime ${replicaBatchTime} --consAlgo ${algo} --pipelineLength ${pipelineLength}  " >${output_path}5.log &
 echo "Started servers"
 
 sleep 10
