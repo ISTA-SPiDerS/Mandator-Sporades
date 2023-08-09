6ebf530a296cb0d70f28d4b96e34fbc88f3d5d44
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-08-09 18:02:40 +0200

    updated test automation to remove pipeline length 1

--- experiments/best-case/test-automation.py
+++ experiments/best-case/test-automation.py
@@ -10,55 +10,50 @@ from performance_extract import *
 setting = sys.argv[1]
 numIter = sys.argv[2]
 
-replicaBatchSize = 3000
+replicaBatchSize = 1000
 replicaBatchTime = 5000
+pipeline = 10
 
 iterations = list(range(1, int(numIter) + 1))
-pipelines = [1, 10]
 
-arrivals = [500, 25000, 35000, 40000, 60000, 80000, 100000]
+arrivals = [500, 10000, 25000, 35000, 40000, 50000]
 
 for iteration in iterations:
     for arrival in arrivals:
-        for pipeline in pipelines:
-            print(str(datetime.now()) + ": iteration: " + str(iteration) + ", arrival:" + str(
-                arrival) + ", pipeline:" + str(pipeline) + "\n")
+        print(str(datetime.now()) + ": iteration: " + str(iteration) + ", arrival:" + str(
+            arrival)+ "\n")
+        sys.stdout.flush()
+        os.system(
+            "/bin/bash experiments/best-case/epaxos.sh " + str(int(arrival)) + " "
+            + str(replicaBatchSize) + " "
+            + str(replicaBatchTime) + " "
+            + setting + " "
+            + str(pipeline) + " "
+            + str(iteration))
+        for algo in ["async", "paxos"]:
             os.system(
-                "/bin/bash experiments/best-case/epaxos.sh " + str(int(arrival)) + " "
+                "/bin/bash experiments/best-case/mandator.sh " + str(int(arrival)) + " "
                 + str(replicaBatchSize) + " "
                 + str(replicaBatchTime) + " "
-                + setting + " "
-                + str(pipeline) + " "
+                + setting + " " + algo + " " + str(3) + " "
                 + str(iteration))
-            for algo in ["async", "paxos"]:
-                if pipeline != 1:
-                    continue
-                os.system(
-                    "/bin/bash experiments/best-case/mandator.sh " + str(int(arrival)) + " "
-                    + str(replicaBatchSize) + " "
-                    + str(replicaBatchTime) + " "
-                    + setting + " " + algo + " " + str(3) + " "
-                    + str(iteration))
-            for algo in ["paxos", "raft"]:
-                if algo == "raft" and pipeline != 1:
-                    continue
-                os.system(
-                    "/bin/bash experiments/best-case/paxos_raft.sh " + str(int(arrival)) + " "
-                    + str(replicaBatchSize) + " "
-                    + str(replicaBatchTime) + " "
-                    + setting + " " + str(pipeline) + " " + algo + " " +
-                    str(iteration))
-
-            if pipeline == 1:
-                os.system(
-                    "/bin/bash experiments/best-case/rabia.sh " + str(int(arrival)) + " "
-                    + str(replicaBatchSize) + " "
-                    + setting + " "
-                    + str(iteration))
-
+        for algo in ["paxos", "raft"]:
             os.system(
-                "/bin/bash experiments/best-case/sporades.sh " + str(int(arrival)) + " "
+                "/bin/bash experiments/best-case/paxos_raft.sh " + str(int(arrival)) + " "
                 + str(replicaBatchSize) + " "
                 + str(replicaBatchTime) + " "
-                + setting + " " + str(3) + " " + str(pipeline) + " "
-                + str(iteration))
+                + setting + " " + str(pipeline) + " " + algo + " " +
+                str(iteration))
+
+        os.system(
+            "/bin/bash experiments/best-case/rabia.sh " + str(int(arrival)) + " "
+            + str(replicaBatchSize) + " "
+            + setting + " "
+            + str(iteration))
+
+        os.system(
+            "/bin/bash experiments/best-case/sporades.sh " + str(int(arrival)) + " "
+            + str(replicaBatchSize) + " "
+            + str(replicaBatchTime) + " "
+            + setting + " " + str(3) + " " + str(pipeline) + " "
+            + str(iteration))
