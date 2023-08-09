126e2fd1c8905e1cb0b1cc057bb71ad2739d2ad2
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-08-09 16:53:45 +0200

    updated new ip.sh

--- experiments/setup-5/ip.sh
+++ experiments/setup-5/ip.sh
@@ -6,13 +6,13 @@ user_name="ubuntu"
 replica1_name=ec2-3-27-60-192.ap-southeast-2.compute.amazonaws.com
 replica1=${user_name}@${replica1_name}
 
-replica2_name=ec2-44-204-33-0.compute-1.amazonaws.com
+replica2_name=ec2-52-196-164-39.ap-northeast-1.compute.amazonaws.com
 replica2=${user_name}@${replica2_name}
 
-replica3_name=ec2-34-240-78-166.eu-west-1.compute.amazonaws.com
+replica3_name=ec2-43-202-58-131.ap-northeast-2.compute.amazonaws.com
 replica3=${user_name}@${replica3_name}
 
-replica4_name=ec2-3-109-153-92.ap-south-1.compute.amazonaws.com
+replica4_name=ec2-13-208-44-189.ap-northeast-3.compute.amazonaws.com
 replica4=${user_name}@${replica4_name}
 
 replica5_name=ec2-54-251-150-217.ap-southeast-1.compute.amazonaws.com
@@ -21,13 +21,13 @@ replica5=${user_name}@${replica5_name}
 client1_name=ec2-13-211-188-163.ap-southeast-2.compute.amazonaws.com
 client1=${user_name}@${client1_name}
 
-client2_name=ec2-3-82-205-206.compute-1.amazonaws.com
+client2_name=ec2-18-183-180-205.ap-northeast-1.compute.amazonaws.com
 client2=${user_name}@${client2_name}
 
-client3_name=ec2-34-243-185-116.eu-west-1.compute.amazonaws.com
+client3_name=ec2-13-124-6-190.ap-northeast-2.compute.amazonaws.com
 client3=${user_name}@${client3_name}
 
-client4_name=ec2-52-66-246-21.ap-south-1.compute.amazonaws.com
+client4_name=ec2-15-152-47-172.ap-northeast-3.compute.amazonaws.com
 client4=${user_name}@${client4_name}
 
 client5_name=ec2-54-251-157-141.ap-southeast-1.compute.amazonaws.com
@@ -37,15 +37,15 @@ declare -a machines=(${replica1} ${replica2} ${replica3} ${replica4} ${replica5}
 echo "ip addresses loaded"
 
 replica1_ip="3.27.60.192"
-replica2_ip="44.204.33.0"
-replica3_ip="34.240.78.166"
-replica4_ip="3.109.153.92"
+replica2_ip="52.196.164.39"
+replica3_ip="43.202.58.131"
+replica4_ip="13.208.44.189"
 replica5_ip="54.251.150.217"
 
 client1_ip="13.211.188.163"
-client2_ip="3.82.205.206"
-client3_ip="34.243.185.116"
-client4_ip="52.66.246.21"
+client2_ip="18.183.180.205"
+client3_ip="13.124.6.190"
+client4_ip="15.152.47.172"
 client5_ip="54.251.157.141"
 
 kill_command="pkill epaxos_master ; pkill epaxos_server; pkill epaxos_client; pkill man_client; pkill man_replica;  pkill pa_ra_replica ; pkill pa_ra_client; pkill pipe_client; pkill pipe_replica; pkill rabia"
