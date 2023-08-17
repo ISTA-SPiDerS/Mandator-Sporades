5d02d32e486f89198a3b51f528263e3df5b6777d
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-08-17 21:35:17 +0200

    increased sleep time after status messages

--- experiments/bash/paxos_raft.sh
+++ experiments/bash/paxos_raft.sh
@@ -49,7 +49,7 @@ sleep 5
 nohup sshpass ssh -o "StrictHostKeyChecking no" -i ${cert} ${client2} ".${remote_ctl_path} --name 22 --config ${remote_config_path} --logFilePath ${remote_log_path} --requestType status --operationType 3" >${output_path}status3.log &
 echo "Sent consensus start up"
 
-sleep 20
+sleep 30
 
 echo "Starting client[s]"
 
