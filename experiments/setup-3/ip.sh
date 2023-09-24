ca8ae91bf6055135b2282a3a55c1e981c53609c7
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2023-09-24 12:38:48 +0200

    edited setup-3

--- experiments/setup-3/ip.sh
+++ experiments/setup-3/ip.sh
@@ -3,34 +3,34 @@ pwd=$(pwd)
 cert=${pwd}/experiments/private_key_aws/pasindu2023
 user_name="ubuntu"
 
-replica1_name=ec2-3-27-149-75.ap-southeast-2.compute.amazonaws.com
+replica1_name=ec2-3-27-164-21.ap-southeast-2.compute.amazonaws.com
 replica1=${user_name}@${replica1_name}
 
-replica2_name=ec2-43-207-113-180.ap-northeast-1.compute.amazonaws.com
+replica2_name=ec2-52-68-193-183.ap-northeast-1.compute.amazonaws.com
 replica2=${user_name}@${replica2_name}
 
-replica3_name=ec2-13-125-5-113.ap-northeast-2.compute.amazonaws.com
+replica3_name=ec2-13-125-56-107.ap-northeast-2.compute.amazonaws.com
 replica3=${user_name}@${replica3_name}
 
-client1_name=ec2-13-238-120-105.ap-southeast-2.compute.amazonaws.com
+client1_name=ec2-54-206-135-165.ap-southeast-2.compute.amazonaws.com
 client1=${user_name}@${client1_name}
 
-client2_name=ec2-18-183-212-228.ap-northeast-1.compute.amazonaws.com
+client2_name=ec2-52-194-247-6.ap-northeast-1.compute.amazonaws.com
 client2=${user_name}@${client2_name}
 
-client3_name=ec2-3-36-119-2.ap-northeast-2.compute.amazonaws.com
+client3_name=ec2-52-79-90-1.ap-northeast-2.compute.amazonaws.com
 client3=${user_name}@${client3_name}
 
 declare -a machines=(${replica1} ${replica2} ${replica3}  ${client1} ${client2} ${client3})
 echo "ip addresses loaded"
 
-replica1_ip="3.27.149.75"
-replica2_ip="43.207.113.180"
-replica3_ip="13.125.5.113"
+replica1_ip="3.27.164.21"
+replica2_ip="52.68.193.183"
+replica3_ip="13.125.56.107"
 
-client1_ip="13.238.120.105"
-client2_ip="18.183.212.228"
-client3_ip="3.36.119.2"
+client1_ip="54.206.135.165"
+client2_ip="52.194.247.6"
+client3_ip="52.79.90.1"
 
 
 kill_command="pkill epaxos_master ; pkill epaxos_server; pkill epaxos_client; pkill man_client; pkill man_replica;  pkill pa_ra_replica ; pkill pa_ra_client; pkill pipe_client; pkill pipe_replica; pkill rabia"
